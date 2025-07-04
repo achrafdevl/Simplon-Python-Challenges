0e49790466ee2eaec12be496f40a20b912d44f94
achraf <achrafchair6@gmail.com>
2025-07-04 12:34:41 +0100

    challenge numpy last update

--- challenges4/challenge_4.py
+++ challenges4/challenge_4.py
@@ -7,7 +7,10 @@ matrice2 =np.array([ [5, 6], [7, 8] ])
 
 multiplication = np.dot(matrice1 , matrice2)
 print(multiplication)
-
+print("*************")
 transpose = np.transpose(multiplication)
-
 print(transpose)
+
+print("*************")
+inverse= np.linalg.inv(multiplication)
+print(inverse)
